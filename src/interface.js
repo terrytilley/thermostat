6686b08df85899f72c135c555cc52ce0a034d605
Terry <terry@terrytilley.com>
2016-08-04 18:45:01 +0100

    Connect jQuery

--- src/interface.js
+++ src/interface.js
@@ -1,4 +1,38 @@
 $(document).ready(function() {
+
   var thermostat = new Thermostat();
   $('#temperature').text(thermostat.temperature);
+
+  function updateTemperature() {
+    $('#temperature').text(thermostat.temperature);
+    $('#temperature').attr('class', thermostat.energyUsage());
+  }
+
+  $('#temp-up').on('click', function() {
+    thermostat.upTemp();
+    updateTemperature();
+  });
+
+  $('#temp-down').on('click', function() {
+    thermostat.downTemp();
+    updateTemperature();
+  });
+
+  $('#temp-reset').on('click', function() {
+    thermostat.resetTemperature();
+    updateTemperature();
+  });
+
+  $('#powersaving-on').click(function() {
+    thermostat.switchPowerSavingModeOn();
+    $('#power-saving-status').text('on');
+    updateTemperature();
+  });
+
+  $('#powersaving-off').click(function() {
+    thermostat.switchPowerSavingModeOff();
+    $('#power-saving-status').text('off');
+    updateTemperature();
+  });
+
 });
